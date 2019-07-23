7372a178983e3f06d4c6410f2ec593c2c5503f08
ppppbamzy <pppp.bamzy@gmail.com>
2019-07-23 12:00:28 +0800

    Update competition code template.cpp

--- competition code template.cpp
+++ competition code template.cpp
@@ -40,9 +40,13 @@ istringstream testIStream;
 
 void genTest() {
     test = string(R"foo(
---|-- |-- /--\ --|--  /--\   /\   |\  /| |--\ |   |--
-  |   |-- `--.   |    `--.  /__\  | \/ | |__/ |   |--
-  |   |__ \__/   |    \__/ /    \ |    | |    |__ |__
+ooooooooooooo oooooooooooo  .oooooo..o ooooooooooooo     .oooooo..o       .o.       ooo        ooooo ooooooooo.   ooooo        oooooooooooo 
+8'   888   `8 `888'     `8 d8P'    `Y8 8'   888   `8    d8P'    `Y8      .888.      `88.       .888' `888   `Y88. `888'        `888'     `8 
+     888       888         Y88bo.           888         Y88bo.          .8"888.      888b     d'888   888   .d88'  888          888         
+     888       888oooo8     `"Y8888o.       888          `"Y8888o.     .8' `888.     8 Y88. .P  888   888ooo88P'   888          888oooo8    
+     888       888    "         `"Y88b      888              `"Y88b   .88ooo8888.    8  `888'   888   888          888          888    "    
+     888       888       o oo     .d8P      888         oo     .d8P  .8'     `888.   8    Y     888   888          888       o  888       o 
+    o888o     o888ooooood8 8""88888P'      o888o        8""88888P'  o88o     o8888o o8o        o888o o888o        o888ooooood8 o888ooooood8 
     )foo");
 }
 #endif
